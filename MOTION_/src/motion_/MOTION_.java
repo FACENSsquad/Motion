8c0d8a26ed6777f5f163fbd661b80592e0ff9e3f
Matheus Maciel <102701486+macielths@users.noreply.github.com>
2022-04-27 21:09:06 -0300

    Update MOTION_.java

--- MOTION_/src/motion_/MOTION_.java
+++ MOTION_/src/motion_/MOTION_.java
@@ -2,18 +2,16 @@
 package motion_;
 
 //matheus
-
-/**
- *aaa
- * @author joaom
- */
 public class MOTION_ {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int num1;
+       int num2;
     }
+    
+    
     
 }
