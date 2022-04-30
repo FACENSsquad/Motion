4d07b1a6f23b6d2af20d2c707cc94b6a26bd7c94
Vinicius Domingues Silva <Viniciusd37@outlook.com>
2022-04-30 18:36:31 -0300

    código de add produto

--- MOTION_/src/BEAN/Produtos_bean.java
+++ MOTION_/src/BEAN/Produtos_bean.java
@@ -4,4 +4,42 @@ package BEAN;
 
 public class Produtos_bean {
     
+    private int id;
+    private String produto;
+    private String tipo;
+    private String ncm;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNcm() {
+        return ncm;
+    }
+
+    public void setNcm(String ncm) {
+        this.ncm = ncm;
+    }
+
+    
 }
--- MOTION_/src/DAO/Produtos_dao.java
+++ MOTION_/src/DAO/Produtos_dao.java
@@ -1,7 +1,41 @@
 
 package DAO;
 
+import BEAN.Produtos_bean;
+import CONEXAO.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 
 public class Produtos_dao {
     
+    private Conexao conexao;
+    private Connection conn;
+    
+    public Produtos_dao(){
+    
+        this.conexao = new Conexao();
+        this.conn = this.conexao.Bd_Conexao();
+    }
+    
+    public void inserir(Produtos_bean produto){
+    
+        String sql = "insert into produto(produto, tipo, ncm) values"
+                   + "(? , ?, ?)";
+        
+        try {
+            
+            PreparedStatement pst = this.conn.prepareStatement(sql);
+            pst.setString(1, produto.getProduto());
+            pst.setString(2, produto.getTipo());
+            pst.setString(2, produto.getNcm());
+            pst.execute();
+            
+        } catch (Exception e) {
+            System.out.println("Erro ao inserir produto");
+        }
+        
+    }
 }
+ 
+
