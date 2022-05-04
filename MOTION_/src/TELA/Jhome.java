231dfb5b8b3024b50e2baa41b0aaf64129b11165
João melo <joaomelo_9@hotmail.com>
2022-05-03 22:11:15 -0300

    att

--- MOTION_/src/TELA/Jhome.java
+++ MOTION_/src/TELA/Jhome.java
@@ -33,11 +33,11 @@ public class Jhome extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1139, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 779, Short.MAX_VALUE)
         );
 
         pack();
